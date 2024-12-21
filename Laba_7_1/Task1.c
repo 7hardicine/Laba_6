b1524ac8d0d21bab9ddcc262cf85e232c21ffcaf
Hardicine <hardicine@mail.ru>
2024-12-21 23:11:45 +0300

    21.12 23:11 Сделал крутую шейкерную сортировку и пошёл спатки

--- Laba_7_1/Task1.c
+++ Laba_7_1/Task1.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <locale.h>
+#include <time.h>
 #define MIN -45
 #define MAX 45
 #define K 1.2473309
@@ -28,26 +29,23 @@ void main()
 
 	// Объявляем массив A и оригинальный адрес
 	int* arr_A = (int*)malloc(size_A * sizeof(int));
-	int* arr_A_orig = arr_A;
 
 	// Заполнение массива A случайными числами
-	printf("\nМассив A:");
-	for (int i = 0; i < size_A; arr_A++, i++)
+	puts("\nМассив A:");
+	for (int i = 0; i < size_A; i++)
 	{
-		*arr_A = rand() % (MAX - MIN + 1) + MIN;
-		printf("\narr_A[%2d] = %d", i + 1, *arr_A);
+		*(arr_A + i) = rand() % (MAX - MIN + 1) + MIN;
+		printf("arr_A[%2d] = %d\n", i + 1, *(arr_A + i));
 	} 
 
-	arr_A = arr_A_orig;
-
 	// Меню
-	puts("\n\nСортировки:");
-	puts("\n1 - Получение упорядоченного по возрастанию массива методом слияния двух упорядоченных массивов,\nодин из которых упорядочен по возрастанию, а другой - по убыванию.");
+	puts("\nСортировки:\n");
+	puts("1 - Получение упорядоченного по возрастанию массива методом слияния двух упорядоченных массивов,\nодин из которых упорядочен по возрастанию, а другой - по убыванию.");
 	puts("2 - Метод сортировки по убыванию обменом  рядом стоящих элементов с фиксированным числом просмотров,\nнаправленных справа налево.");
 	puts("3 - Шейкерная сортировка по убыванию");
 	puts("4 - Сортировка расчёской по возрастанию");
-	puts("5 - Метод сортировки по убыванию извлечением максимального элемента,\nизвлечение максимального элемента проводить слева направо");
-	puts("\nВведите номер интересующей вас сортировки:");
+	puts("5 - Метод сортировки по убыванию извлечением максимального элемента,\nизвлечение максимального элемента проводить слева направо\n");
+	puts("Введите номер интересующей вас сортировки:");
 
 	// Считываем номер выбранного способа сортировки
 	int sort;
@@ -74,14 +72,13 @@ void main()
 
 		// Объявляем массив B и оригинальный адрес
 		int* arr_B = (int*)malloc(size_B * sizeof(int));
-		int* arr_B_orig = arr_B;
 
 		// Заполнение массива B случайными числами
 		puts("\nМассив B:");
-		for (int i = 0; i < size_B; arr_B++, i++)
+		for (int i = 0; i < size_B; i++)
 		{
-			*arr_B = rand() % (MAX - MIN + 1) + MIN;
-			printf("arr_B[%2d] = %d\n", i + 1, *arr_B);
+			*(arr_B + i) = rand() % (MAX - MIN + 1) + MIN;
+			printf("arr_B[%2d] = %d\n", i + 1, *(arr_B + i));
 		}
 
 		// Объявляем необходимые переменные для сортировки расчёской
@@ -92,50 +89,47 @@ void main()
 		// Сортировка расчёской массива А от меньшего к большему (начало)
 		while (step_A > 1) 
 		{
-			for (int i = 0; i < size_A; arr_A++, i++)
+			for (int i = 0; i < size_A; i++)
 			{
-				if (*arr_A < *(arr_A + step_A))
+				if (*(arr_A + i) < *(arr_A + i + step_A))
 				{
-					temp = *arr_A;
-					*arr_A = *(arr_A + step_A);
-					*(arr_A + step_A) = temp;
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + step_A + i);
+					*(arr_A + step_A + i) = temp;
 				}
 				step_A /= K;
 			}
-			arr_A = arr_A_orig;
 		}
 
 		// Сортировка расчёской массива А от меньшего к большему (конец)
 		while (flag) 
 		{
 			flag = 0;
-			for (int i = 0; i < size_A - 1; arr_A++, i++)
+			for (int i = 0; i < size_A - 1; i++)
 			{
-				if (*arr_A > *(arr_A + 1))
+				if (*(arr_A + i) > *(arr_A + i + 1))
 				{
-					temp = *arr_A;
-					*arr_A = *(arr_A + 1);
-					*(arr_A + 1) = temp;
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + i + 1);
+					*(arr_A + i + 1) = temp;
 					flag = 1;
 				}
 			}
-			arr_A = arr_A_orig;
 		}
 
 		// Сортировка расчёской массива B от большего к меньшему (начало)
 		while (step_B > 1)
 		{
-			for (int i = 0; i < size_B; arr_B++, i++)
+			for (int i = 0; i < size_B; i++)
 			{
-				if (*arr_B > *(arr_B + step_B))
+				if (*(arr_B + i) > *(arr_B + i + step_B))
 				{
-					temp = *arr_B;
-					*arr_B = *(arr_B + step_B);
-					*(arr_B + step_B) = temp;
+					temp = *(arr_B + i);
+					*(arr_B + i) = *(arr_B + i + step_B);
+					*(arr_B + i + step_B) = temp;
 				}
 				step_B /= K;
 			}
-			arr_B = arr_B_orig;
 		}
 
 		flag++;
@@ -144,87 +138,80 @@ void main()
 		while (flag)
 		{
 			flag = 0;
-			for (int i = 0; i < size_B - 1; arr_B++, i++)
+			for (int i = 0; i < size_B - 1; i++)
 			{
-				if (*arr_B < *(arr_B + 1))
+				if (*(arr_B + i) < *(arr_B + i + 1))
 				{
-					temp = *arr_B;
-					*arr_B = *(arr_B + 1);
-					*(arr_B + 1) = temp;
+					temp = *(arr_B + i);
+					*(arr_B + i) = *(arr_B + i + 1);
+					*(arr_B + i + 1) = temp;
 					flag = 1;
 				}
 			}
-			arr_B = arr_B_orig;
 		}
 
 		// Вывод отсортированного массива A
 		puts("\nОтсортированный массив A:");
-		for (int i = 0; i < size_A; arr_A++, i++)
+		for (int i = 0; i < size_A; i++)
 		{
-			printf("arr_A[%2d] = %d\n", i + 1, *arr_A);
+			printf("arr_A[%2d] = %d\n", i + 1, *(arr_A + i));
 		}
 
 		// Вывод отсортированного массива B
 		puts("\nОтсортированный массив B:");
-		for (int i = 0; i < size_B; arr_B++, i++)
+		for (int i = 0; i < size_B; i++)
 		{
-			printf("arr_B[%2d] = %d\n", i + 1, *arr_B);
+			printf("arr_B[%2d] = %d\n", i + 1, *(arr_B + i));
 		}
 
 		// Объявляем новый массив
 		int* arr_C = (int*)malloc((size_B + size_A) * sizeof(int));
-		int* arr_C_orig = arr_C;
 
 		// Объявляем переменные для сортировки слиянием
-		arr_B = arr_B_orig + size_B - 1;
-		arr_A = arr_A_orig;
+		int j = size_B - 1;
+		int i = 0;
 
 		// Выполняем сортировку слиянием
-		for (arr_C; arr_C < arr_C_orig + size_A + size_B; arr_C++)
+		for (int l = 0; l < size_A + size_B; l++)
 		{
-			if (arr_A < arr_A_orig + size_A)
+			if (i < size_A)
 			{
-				if (arr_B >= arr_B_orig)
+				if (j >= 0)
 				{
-					if (*arr_A <= *arr_B)
+					if (*(arr_A + i) <= *(arr_B + j))
 					{
-						*arr_C = *arr_A;
-						arr_A++;
+						*(arr_C + l) = *(arr_A + i);
+						i++;
 					}
 					else
 					{
-						*arr_C = *arr_B;
-						arr_B--;
+						*(arr_C + l) = *(arr_B + j);
+						j--;
 					}
 				}
 				else
 				{
-					*arr_C = *arr_A;
-					arr_A++;
+					*(arr_C + l) = *(arr_A + i);
+					i++;
 				}
 			}
 			else
 			{
-				*arr_C = *arr_B;
-				arr_B--;
+				*(arr_C + l) = *(arr_B + j);
+				j--;
 			}
 		}
 
-		arr_A = arr_A_orig;
 		free(arr_A);
-		arr_B = arr_B_orig;
 		free(arr_B);
-
-		arr_C = arr_C_orig;
 
 		// Выводим массив C
 		puts("\nМассив C, образованный слиянием массивов A и B:");
-		for (int i = 0; i < size_A + size_B; arr_C++, i++)
+		for (int i = 0; i < size_A + size_B; i++)
 		{
-			printf("arr_C[%2d] = %d\n", i + 1, *arr_C);
+			printf("arr_C[%2d] = %d\n", i + 1, *(arr_C + i));
 		}
 
-		arr_C = arr_C_orig;
 		free(arr_C);
 
 		break;
@@ -236,7 +223,45 @@ void main()
 	}
 	case 3:
 	{
-		// Типо третья сортировка
+		int left = 0;
+		int right = size_A - 1;
+		int flag = 1;
+		int temp;
+
+		while ((left < right) && (flag = 1))
+		{
+			flag = 0;
+			for (int i = left; i < right; i++)
+			{
+				if (*(arr_A + i) < *(arr_A + i + 1))
+				{
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + i + 1);
+					*(arr_A + i + 1) = temp;
+					flag = 1;
+				}
+			}
+			right--;
+			for (int i = right; i > left; i--)
+			{
+				if (*(arr_A + i - 1) < *(arr_A + i))
+				{
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + i - 1);
+					*(arr_A + i - 1) = temp;
+					flag = 1;
+				}
+			}
+			left++;
+		}
+
+		// Вывод отсортированного массива A
+		puts("\nОтсортированный массив A:");
+		for (int i = 0; i < size_A; i++)
+		{
+			printf("arr_A[%2d] = %d\n", i + 1, *(arr_A + i));
+		}
+
 		break;
 	}
 	case 4:
@@ -248,41 +273,39 @@ void main()
 		// Сортировка расчёской массива А от меньшего к большему (начало)
 		while (step_A > 1)
 		{
-			for (int i = 0; i < size_A; arr_A++, i++)
+			for (int i = 0; i < size_A; i++)
 			{
-				if (*arr_A < *(arr_A + step_A))
+				if (*(arr_A + i) < *(arr_A + i + step_A))
 				{
-					temp = *arr_A;
-					*arr_A = *(arr_A + step_A);
-					*(arr_A + step_A) = temp;
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + i + step_A);
+					*(arr_A + i + step_A) = temp;
 				}
 				step_A /= K;
 			}
-			arr_A = arr_A_orig;
 		}
 
 		// Сортировка расчёской массива А от меньшего к большему (конец)
 		while (flag)
 		{
 			flag = 0;
-			for (int i = 0; i < size_A - 1; arr_A++, i++)
+			for (int i = 0; i < size_A - 1; i++)
 			{
-				if (*arr_A > *(arr_A + 1))
+				if (*(arr_A + i) > *(arr_A + i + 1))
 				{
-					temp = *arr_A;
-					*arr_A = *(arr_A + 1);
-					*(arr_A + 1) = temp;
+					temp = *(arr_A + i);
+					*(arr_A + i) = *(arr_A + i + 1);
+					*(arr_A + i + 1) = temp;
 					flag = 1;
 				}
 			}
-			arr_A = arr_A_orig;
 		}
 
 		// Вывод отсортированного массива A
 		puts("\nОтсортированный массив A:");
 		for (int i = 0; i < size_A; i++)
 		{
-			printf("arr_A[%2d] = %d\n", i + 1, arr_A[i]);
+			printf("arr_A[%2d] = %d\n", i + 1, *(arr_A + i));
 		}
 
 		break;
